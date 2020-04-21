0f43bfe65dd3f7c659ee9873161186e99864f48f
FBo-dot <fabsailor@gmail.com>
2020-04-22 00:00:51 +0200

    Datetime and Time WTF fields tests

--- form_handling/app.py
+++ form_handling/app.py
@@ -7,8 +7,10 @@ Created on Mon Apr 20 14:30:40 2020
 
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, TimeField
 from wtforms.validators import InputRequired, Length, AnyOf
+from wtforms.fields.html5 import DateField
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Thisisasecret!'
@@ -20,13 +22,23 @@ class LoginForm(FlaskForm):
     password = PasswordField('password',
                              validators=[InputRequired('Password is required!'),
                                          AnyOf(values=['password', 'secret'])])
+    dep_date = DateField('departure date', format='%Y-%m-%d',
+                         validators=[InputRequired('A departure date is required')])
+    dep_time = TimeField('departure time', format='%H:%M',
+                         validators=[InputRequired('Time required as hh:mm')])
 
 @app.route('/form', methods=['GET', 'POST'])
 def form():
     form = LoginForm()
     
     if form.validate_on_submit():
-        return '<h1>The username is {}. The password is {}.'.format(form.username.data, form.password.data)
+        dep_datetime = datetime.combine(form.dep_date.data, form.dep_time.data)
+#        dep_datetime = datetime.combine(datetime.strptime(form.dep_date.data, '%Y-%m-%d'), datetime.strptime(form.dep_time.data, '%H:%M:S'))
+        dep_datetime_str = dep_datetime.strftime('%Y-%m-%d %H:%M')
+        result_str = '<h1>The username is {}. The password is {}.'.format(form.username.data, form.password.data)
+#        result_str = result_str + ' The departure date is {}. The departure time is {}.'.format(form.dep_date.data, form.dep_time.data)
+        result_str = result_str + 'The departure date and time is {}.'.format(dep_datetime_str)
+        return  result_str
     return render_template('form.html', form=form)
 
 if __name__ == '__main__':
